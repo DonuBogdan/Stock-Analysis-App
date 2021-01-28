8c706c19994a4ee1ad2cda072aa4c9135849ecee
Donu Bogdan <donu_bogdan_andrei@yahoo.com>
2021-01-29 01:38:35 +0200

    Modified fe.

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fe';
+  
+  listOfItems = [1, 2, 3, 4, 5]
+  response = []
+
+  constructor(private http: HttpClient) { }
+
+  helloWorld() {
+    return this.http.get<[]>('http://127.0.0.1:5000/').subscribe((res) => this.response = res)
+  }
+
 }
