7ef3f67422233b60d62e6c26c6116fadacd84641
Donu Bogdan <donu_bogdan_andrei@yahoo.com>
2021-03-26 20:59:11 +0200

    Removed credentials.

--- be/tweets.py
+++ be/tweets.py
@@ -6,10 +6,10 @@ def get_tweets_text(search_word):
     print('Search word:', search_word)
 
     # credentials
-    consumer_key = 'R1LZFgXrFclzdSh0GupX2pxnT'
-    consumer_secret = 'bBgSvyYmnN0PmG5Crq9nnhCr0pKhCImyfm0XFkUnRmbK4IRH3x'
-    access_token = '1336286674222002177-305av71Wo8u53WMxF6wUPEQrQdDa8j'
-    access_token_secret = 'rgMirfblF1SFXnP5dfBxwcmrQwdQQjdJX7o8zkhj8RLY2'
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = '-'
+    access_token_secret = ''
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
